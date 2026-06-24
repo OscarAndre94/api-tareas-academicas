3050ad41b3116746c90c7d5e81497341291a2f4d
Oscar Llamuca <oscarandre998@gmail.com>
2026-06-24 17:48:03 -0500

    feat: Se agrego las rutas para las diferentes funcionalidades de las tareas {obtenerId, actualizarId y eliminarId} probado con éxito

--- routes/tareas.routes.js
+++ routes/tareas.routes.js
@@ -5,5 +5,8 @@ const tareaController = require('../controllers/tarea.controller');
 //Enlazmos las rutas
 router.post('/', tareaController.crearTarea);
 router.get('/', tareaController.obtenerTareas);
+router.get('/:id', tareaController.obtenerTareaId);
+router.put('/:id', tareaController.actualizarTareaId);
+router.delete('/:id', tareaController.eliminarTarea);
 
 module.exports = router;

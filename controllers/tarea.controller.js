ca4e0d58e7703f30b6b5e7955629ca2d2a8f864c
Oscar Llamuca <oscarandre998@gmail.com>
2026-06-24 17:46:43 -0500

    feat: se creo las diferentes funcionalidades obtener por id, actualizar por id y eliminar por id las tareas

--- controllers/tarea.controller.js
+++ controllers/tarea.controller.js
@@ -40,3 +40,63 @@ exports.obtenerTareas = async(req, res) => {
         });
     }
 };
+
+//Obtener tarea por ID
+exports.obtenerTareaId = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const tarea = await Tarea.findById(id);
+
+        if(!tarea){
+            return res.status(404).json({
+                mensaje: "Tarea no encontrada"
+            });
+        }
+        return res.status(200).json(tarea);
+    }catch(error){
+        return res.status(500).json({
+            error: error.message,
+            mensaje: "Error al buscar la tarea"
+        });
+    }
+};
+
+//Actualizar tarea por ID
+exports.actualizarTareaId = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const tareaActualizada = await Tarea.findByIdAndUpdate(id, req.body, { new: true });
+
+        if(!tareaActualizada){
+            return res.status(404).json({
+                mensaje: "No se pudo actualizar, tarea no encontrada"
+            });
+        }
+        return res.status(200).json(tareaActualizada);
+    }catch(error){
+        return res.status(500).json({
+            error: error.message,
+            mensaje: "Error al actualizar la tarea"
+        });
+    }
+};
+
+//Eliminar tarea
+exports.eliminarTarea = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const tareaEliminada = await Tarea.findByIdAndDelete(id);
+
+        if(!tareaEliminada){
+            return res.status(404).json({
+                mensaje: "No se pudo eliminar, tarea no encontrada"
+        });
+       }
+       return res.status(200).json(tareaEliminada);
+    }catch(error){
+        return res.status(500).json({
+            error: error.message,
+            mensaje: "Error al eliminar la tarea"
+        });        
+    }
+};

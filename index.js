7c7b327bb5f1427f2c25f96f5b7aba557a598823
Oscar Llamuca <oscarandre998@gmail.com>
2026-06-24 12:24:42 -0500

    feat: Configuración de dependencias y creación del servidor en node

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Inicializado API para tareas academicas universitarias");
+require('dotenv').config();
+
+//Importar dependencias
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+
+//Crear el servidor
+const app = express();
+const PORT = process.env.PORT || 4000; // Si no encuentra el .env usará por defecto el puerto 4000
+
+//Configuración del cors
+app.use(cors());
+
+//Convertir los datos del body a objetos
+app.use(express.json()); //Con este middleware analizamos las solicitudes que nos lleguen en formato JSON
+app.use(express.urlencoded({extended: true}));
+
+//Servidor a escuchar
+app.listen(PORT, () => {
+    console.log("Servidor corriendo correctamente en el puerto "+PORT);
+});
+
+
+
+

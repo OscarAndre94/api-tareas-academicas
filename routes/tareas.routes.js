f26ca2623c07e5d48f96aa5dcde1c714849913e5
Oscar Llamuca <oscarandre998@gmail.com>
2026-06-24 17:23:56 -0500

    feat: Se agrego la ruta para obtener las tareas y se realizo las prubeas con exito

--- routes/tareas.routes.js
+++ routes/tareas.routes.js
@@ -4,5 +4,6 @@ const tareaController = require('../controllers/tarea.controller');
 
 //Enlazmos las rutas
 router.post('/', tareaController.crearTarea);
+router.get('/', tareaController.obtenerTareas);
 
 module.exports = router;

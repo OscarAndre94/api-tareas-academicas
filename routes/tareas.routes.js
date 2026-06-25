d0f00bfd9c9121b619be51cd0ed77d0747c06042
Oscar Llamuca <oscarandre998@gmail.com>
2026-06-24 22:59:25 -0500

    feat: Se enlazo la ruta pra la subida de archivos

--- routes/tareas.routes.js
+++ routes/tareas.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const tareaController = require('../controllers/tarea.controller');
+const upload = require('../midlewares/upload.midleware');
 
 //Enlazmos las rutas
 router.post('/', tareaController.crearTarea);
@@ -8,5 +9,6 @@ router.get('/', tareaController.obtenerTareas);
 router.get('/:id', tareaController.obtenerTareaId);
 router.put('/:id', tareaController.actualizarTareaId);
 router.delete('/:id', tareaController.eliminarTarea);
+router.post('/upload-file', upload.single('archivo'), tareaController.subirArchivo);
 
 module.exports = router;
